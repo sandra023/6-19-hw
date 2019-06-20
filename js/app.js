5338458f727db65619ba643ef88ad5d9c080730f
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-19 18:20:41 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -11,11 +11,28 @@
 // }
 // //Question 3
 
-const arry = ["...human...why you taking pictures of me?", "...the catnip made me do it...", "...why does the red dot always get away..."];
-for (i=1; i <=20; i++){
-	console.log("Love me, pet me! HSSSSSSS!");
-	if(i%2 == 0){
+// const arry = ["...human...why you taking pictures of me?", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// for (i=1; i <=20; i++){
+// 	console.log("Love me, pet me! HSSSSSSS!");
+// 	if(i%2 == 0){
 
-		console.log(arry[(Math.floor(Math.random()*arry.length))]);
-	}
-}
+// 		console.log(arry[(Math.floor(Math.random()*arry.length))]);
+// 	}
+// }
+
+//Question 4
+
+for (let i = 1; i <=100; i++){
+		
+		if(i%3 ==0 && i%5 == 0){
+			console.log("FizzBuzz");
+		} else if (i%3 === 0) { 
+			console.log("Fizz");
+		} else if(i%5 == 0) {
+			console.log("Buzz");
+		} else {
+			console.log(i);
+		}
+		}
+
+//Question 5
