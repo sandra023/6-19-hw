a4f955e46b462ed582ec48ed0d06533fcc0fa5f3
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-19 19:11:10 -0600

    Commit 6 - Yell at the Ninja Turtles answered

--- js/app.js
+++ js/app.js
@@ -35,32 +35,34 @@
 // 		}
 // 		}
 
-//Question 5
-const kenny = ["Kenny", 1000, "Austin"];
-const jimHaff = ["Jim H", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const jimClark = ["Jim C", 186, "LA"];
-const ryan = ["Ryan", 65, "Denver"];
+// //Question 5
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
 
-kenny[0] = "Gameboy";
-console.log(kenny);
-jimClark[1] = jimClark[1]+1;
-console.log(jimClark);
-ryan[2] = "Gotham City";
-console.log(ryan);
-reuben.pop();
-console.log(reuben);
-reuben.push("Chicago");
-console.log(reuben);
-jimHaff.pop();
-console.log(jimHaff);
-jimHaff.push("Vegas","Dallas","Tokyo");
-console.log(jimHaff);
-jimHaff.splice(2,1);
-console.log(jimHaff);
+// kenny[0] = "Gameboy";
+// console.log(kenny);
+// jimClark[1] = jimClark[1]+1;
+// console.log(jimClark);
+// ryan[2] = "Gotham City";
+// console.log(ryan);
+// reuben.pop();
+// console.log(reuben);
+// reuben.push("Chicago");
+// console.log(reuben);
+// jimHaff.pop();
+// console.log(jimHaff);
+// jimHaff.push("Vegas","Dallas","Tokyo");
+// console.log(jimHaff);
+// jimHaff.splice(2,1);
+// console.log(jimHaff);
 
-
-
+const turtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+for (let i = 0; i < turtles.length; i++) {
+	console.log(turtles[i].toUpperCase());
+}
 
 
 
