76852e94fed19c6d44593b5eb6395433051aa994
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-19 18:54:42 -0600

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -22,17 +22,45 @@
 
 //Question 4
 
-for (let i = 1; i <=100; i++){
+// for (let i = 1; i <=100; i++){
 		
-		if(i%3 ==0 && i%5 == 0){
-			console.log("FizzBuzz");
-		} else if (i%3 === 0) { 
-			console.log("Fizz");
-		} else if(i%5 == 0) {
-			console.log("Buzz");
-		} else {
-			console.log(i);
-		}
-		}
+// 		if(i%3 ==0 && i%5 == 0){
+// 			console.log("FizzBuzz");
+// 		} else if (i%3 === 0) { 
+// 			console.log("Fizz");
+// 		} else if(i%5 == 0) {
+// 			console.log("Buzz");
+// 		} else {
+// 			console.log(i);
+// 		}
+// 		}
 
 //Question 5
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = "Gameboy";
+console.log(kenny);
+jimClark[1] = jimClark[1]+1;
+console.log(jimClark);
+ryan[2] = "Gotham City";
+console.log(ryan);
+reuben.pop();
+console.log(reuben);
+reuben.push("Chicago");
+console.log(reuben);
+jimHaff.pop();
+console.log(jimHaff);
+jimHaff.push("Vegas","Dallas","Tokyo");
+console.log(jimHaff);
+jimHaff.splice(2,1);
+console.log(jimHaff);
+
+
+
+
+
+
